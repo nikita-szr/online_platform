ddc122ec0c97701f702a5c1570d89e27a381965b
nikitt <nikittaosipov@yandex.ru>
2024-11-29 17:10:34 +0400

    поле вывода количества уроков

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,9 +3,14 @@ from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['id', 'title', 'preview', 'description', 'lesson_count']
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
