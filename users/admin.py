c877810b01c93789a974b99941151e180a2a1fe7
nikitt <nikittaosipov@yandex.ru>
2024-11-29 17:31:47 +0400

    фикстуры для заполнения таблиц

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Payment
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'course', 'lesson', 'amount', 'payment_method')
+    list_filter = ('payment_method',)
