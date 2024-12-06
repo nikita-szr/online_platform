4e92dfdbd666082ee413c944fcbc85b61623bf96
nikitt <nikittaosipov@yandex.ru>
2024-12-06 19:09:48 +0400

    ограничение на создание объектов модераторами

--- courses/views.py
+++ courses/views.py
@@ -4,6 +4,7 @@ from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 from users.permissions import IsModerator
 from .permissions import IsOwnerOrModerator
+from rest_framework.exceptions import PermissionDenied
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -12,6 +13,8 @@ class CourseViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwnerOrModerator]
 
     def perform_create(self, serializer):
+        if self.request.user.groups.filter(name='moderators').exists():
+            raise PermissionDenied("Модераторы не имеют права создавать курсы.")
         serializer.save(owner=self.request.user)
 
     def get_queryset(self):
@@ -26,6 +29,8 @@ class LessonViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwnerOrModerator]
 
     def perform_create(self, serializer):
+        if self.request.user.groups.filter(name='moderators').exists():
+            raise PermissionDenied("Модераторы не имеют права создавать уроки.")
         serializer.save(owner=self.request.user)
 
     def get_queryset(self):
